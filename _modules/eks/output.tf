8cffc1eb77eb99188bfb14d719b2ce590506405d
quyennguyen <quyennv@quyennv.com>
2024-09-25 00:06:42 +0700

    remove output

--- _modules/eks/output.tf
+++ _modules/eks/output.tf
@@ -1,14 +1,1 @@
 
-output "aws_eks_cluster" {
-  value = module.eks.cluster_id
-}
-output "aws_eks_cluster_auth" {
-  value = module.eks.cluster_id
-}
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-output "certificate_authority" {
-  value = module.eks.cluster_certificate_authority_data.0.data
-}
-
