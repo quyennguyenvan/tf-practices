df08c037de72f28b34d179bc929ae76cdc3e830f
quyennguyen <quyennv@quyennv.com>
2024-09-24 23:31:21 +0700

    udpate correct type of value

--- _modules/eks/main.tf
+++ _modules/eks/main.tf
@@ -28,7 +28,7 @@ module "eks" {
     this = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = [var.instance_types]
+      instance_types = var.instance_types
 
       min_size     = var.min_size
       max_size     = var.max_size
