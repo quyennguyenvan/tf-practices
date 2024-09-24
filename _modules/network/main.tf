d8e819004d66dc3145080a9ae878f9c152e020ed
quyennguyen <quyennv@quyennv.com>
2024-09-24 23:00:41 +0700

    update vpc moudle module

--- _modules/network/main.tf
+++ _modules/network/main.tf
@@ -4,15 +4,18 @@ locals {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0"
+  version = "5.13.0"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
-  intra_subnets   = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 52)]
+  azs                 = local.azs
+  private_subnets     = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  public_subnets      = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 4)]
+  database_subnets    = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 8)]
+  elasticache_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 12)]
+  redshift_subnets    = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 16)]
+  intra_subnets       = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 20)]
 
   enable_nat_gateway     = var.enable_nat_gateway
   single_nat_gateway     = var.single_nat_gateway
@@ -23,9 +26,12 @@ module "vpc" {
   create_database_subnet_route_table     = var.create_database_subnet_route_table
   create_database_internet_gateway_route = var.create_database_internet_gateway_route
 
-  enable_flow_log                      = var.enable_flow_log
-  create_flow_log_cloudwatch_iam_role  = var.create_flow_log_cloudwatch_iam_role
-  create_flow_log_cloudwatch_log_group = var.create_flow_log_cloudwatch_log_group
+  enable_flow_log                       = var.enable_flow_log
+  vpc_flow_log_iam_role_name            = "${var.vpc_name}-follow-log-role"
+  vpc_flow_log_iam_role_use_name_prefix = false
+  create_flow_log_cloudwatch_iam_role   = var.create_flow_log_cloudwatch_iam_role
+  create_flow_log_cloudwatch_log_group  = var.create_flow_log_cloudwatch_log_group
+  flow_log_max_aggregation_interval     = 60
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
