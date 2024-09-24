8cb672daa502f834e6157698a5df22fbe799f59d
quyennguyen <quyennv@quyennv.com>
2024-09-24 23:44:48 +0700

    remove data

--- _modules/eks/data.tf
+++ _modules/eks/data.tf
@@ -1,11 +1,2 @@
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
-
-
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_id
-}
