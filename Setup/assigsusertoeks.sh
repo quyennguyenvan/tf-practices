15d70efe231f381a02d1f12b12cf4559a22606df
quyennguyen <quyennv@quyennv.com>
2024-09-25 21:13:48 +0700

    udpate config for setup role and remove the resources no used, please udpate the name of stack to prevent the conflict

--- Setup/assigsusertoeks.sh
+++ Setup/assigsusertoeks.sh
@@ -5,9 +5,24 @@
 AWS_REGION=us-west-2
 AWS_PROFILE=vti
 EKS_CLUSTER_NAME=qnveks
-AWS_USER_ARN=arn:aws:iam::084375555299:user/DE000037
+AWS_USER_ARN=arn:aws:iam::084375555299:user/quyennv_user
+AWS_USERNAME=quyennv_user
 TYPE_ACCESS=cluster
 POLICY_ARN=arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy
 
+#create the resources to access
+echo "Create the access to eks cluster"
+ aws eks create-access-entry --cluster-name $EKS_CLUSTER_NAME \
+    --principal-arn $AWS_USER_ARN --type STANDARD --username $AWS_USERNAME --profile $AWS_PROFILE --region $AWS_REGION
+
 echo "setup user to access eks cluster"
-aws eks associate-access-policy --cluster-name $EKS_CLUSTER_NAME --region $AWS_REGION  --profile $AWS_PROFILE --principal-arn $AWS_USER_ARN --access-scope type=$TYPE_ACCESS  --policy-arn $POLICY_ARN
+aws eks associate-access-policy --cluster-name $EKS_CLUSTER_NAME \
+    --region $AWS_REGION  --profile $AWS_PROFILE --principal-arn $AWS_USER_ARN --access-scope type=$TYPE_ACCESS  --policy-arn $POLICY_ARN
+
+
+# aws eks create-access-entry --cluster-name qnveks --principal-arn arn:aws:iam::084375555299:user/DE000037 --type STANDARD --user DE000037 --kubernetes-groups Viewers --profile vti --region us-west-2
+
+# aws eks delete-access-entry --cluster-name qnveks --principal-arn arn:aws:iam::084375555299:user/DE000037 --profile vti --region us-west-2
+
+
+# aws eks create-access-entry --cluster-name qnveks --principal-arn arn:aws:iam::084375555299:user/DE000037 --type STANDARD --username DE000037 --profile vti --region us-west-2
